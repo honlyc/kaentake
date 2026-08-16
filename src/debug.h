e4152314327a3be11ba75fc707a5163ddf0bc7fd
luckyhhe <luckyhhe@tencent.com>
2026-08-16 21:44:09 +0800

    fix: 调整 log 输出

--- src/debug.h
+++ src/debug.h
@@ -1,12 +1,17 @@
 #pragma once
 
-#ifdef _DEBUG
-#define DEBUG_MESSAGE(FORMAT, ...) DebugMessage(FORMAT, __VA_ARGS__)
-#else
-#define DEBUG_MESSAGE(FORMAT, ...)
-#endif
+#include <cstdio>
 
+#define LOG_INFO(FORMAT, ...) LogMessage(LOG_LEVEL_INFO, FORMAT, __VA_ARGS__)
+#define LOG_DEBUG(FORMAT, ...) LogMessage(LOG_LEVEL_DEBUG, FORMAT, __VA_ARGS__)
+#define LOG_ERROR(FORMAT, ...) LogMessage(LOG_LEVEL_ERROR, FORMAT, __VA_ARGS__)
+#define LOG_WARN(FORMAT, ...) LogMessage(LOG_LEVEL_INFO, "[WARN] " FORMAT, __VA_ARGS__)
 
-void DebugMessage(const char* sFormat, ...);
+typedef enum {
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_ERROR
+} LogLevel;
 
+void LogMessage(LogLevel level, const char* sFormat, ...);
 void ErrorMessage(const char* sFormat, ...);
