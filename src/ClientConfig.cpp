71ce2743a8c42de727bdffee6904376fd610279a
luckyhhe <luckyhhe@tencent.com>
2026-08-16 23:12:38 +0800

    feat: read ClientConfig patches from config.ini [client] section

--- src/ClientConfig.cpp
+++ src/ClientConfig.cpp
@@ -28,11 +28,64 @@ int ClientConfig::serverIP_Port = 8484;
 bool ClientConfig::talkRepeat = false;
 int ClientConfig::talkTime = 2000;
 
+// 补丁分组开关（从 config.ini [client] 读取，1=启用 0=禁用）
+static bool g_EnableManifestAdmin = true;
+static bool g_EnableServerIP = true;
+static bool g_EnableCaps = true;
+static bool g_EnableSkillTweaks = true;
+static bool g_EnableWindowLogos = true;
+static bool g_EnableClimbSpeed = true;
+
+static bool GetBool(const char* key, bool defVal) {
+    char sBuffer[32];
+    char sDef[8];
+    StringCbPrintfA(sDef, sizeof(sDef), "%d", defVal ? 1 : 0);
+    GetPrivateProfileStringA("client", key, sDef, sBuffer, sizeof(sBuffer), ".\\" CONSTANTS_CONFIG_NAME);
+    return atoi(sBuffer) != 0;
+}
+
+static int GetInt(const char* key, int defVal) {
+    char sBuffer[32];
+    char sDef[16];
+    StringCbPrintfA(sDef, sizeof(sDef), "%d", defVal);
+    GetPrivateProfileStringA("client", key, sDef, sBuffer, sizeof(sBuffer), ".\\" CONSTANTS_CONFIG_NAME);
+    return atoi(sBuffer);
+}
+
 void ClientConfig::Init() {
+    // 读取功能开关
+    g_EnableManifestAdmin = GetBool("patch_manifest", true);
+    g_EnableServerIP      = GetBool("patch_server_ip", true);
+    g_EnableCaps          = GetBool("patch_caps", true);
+    g_EnableSkillTweaks   = GetBool("patch_skill_tweaks", true);
+    g_EnableWindowLogos   = GetBool("patch_window_logos", true);
+    g_EnableClimbSpeed    = GetBool("patch_climb_speed", true);
+
+    // 读取功能参数
+    WindowedMode     = GetBool("windowed_mode", true);
+    RemoveLogos      = GetBool("remove_logos", true);
+    useTubi          = GetBool("use_tubi", false);
+    setDamageCap     = GetInt("damage_cap", 19999999);
+    setMAtkCap       = GetInt("matk_cap", 1999999);
+    setAtkCap        = GetInt("atk_cap", 1999999);
+    setAccCap        = GetInt("acc_cap", 999);
+    setAvdCap        = GetInt("avd_cap", 999);
+    speedMovementCap = GetInt("speed_movement_cap", 140);
+    serverIP_Port    = GetInt("server_port", 8484);
+
+    char sIP[64];
+    GetPrivateProfileStringA("client", "server_ip", "127.0.0.1", sIP, sizeof(sIP), ".\\" CONSTANTS_CONFIG_NAME);
+    ServerIP_Address = sIP;
+
+    char sClimb[32];
+    GetPrivateProfileStringA("client", "climb_speed", "3.0", sClimb, sizeof(sClimb), ".\\" CONSTANTS_CONFIG_NAME);
+    climbSpeed = static_cast<float>(atof(sClimb));
+
     ApplyPatches();
 }
 
 void ClientConfig::ApplyPatches() {
+    if (g_EnableManifestAdmin) {
     LOG_DEBUG("ApplyPatches: stage 1 (manifest/admin)");
     FillBytes(0x00C08459, 0x20, 0x00C0846E - 0x00C08459);
     Patch1(0x00C08459, 0x22);
@@ -41,7 +94,9 @@ void ClientConfig::ApplyPatches() {
     Patch1(0x0049C2CD + 1, 0x01);
     Patch1(0x0049CFE8 + 1, 0x01);
     Patch1(0x0049D398 + 1, 0x01);
+    }
 
+    if (g_EnableServerIP) {
     LOG_DEBUG("ApplyPatches: stage 2 (server IP)");
     FillBytes(0x00AFE084, 0x00, 0x006FE0B2 - 0x006FE084);
     const char* serverIP = ServerIP_Address.c_str();
@@ -49,12 +104,14 @@ void ClientConfig::ApplyPatches() {
     PatchStr(0x00AFE084 + 16, serverIP);
     PatchStr(0x00AFE084 + 32, serverIP);
     Patch4(0x007519C1 + 1, serverIP_Port);
+    }
 
     if (useTubi) {
         LOG_DEBUG("ApplyPatches: tubi");
         FillBytes(0x00485C32, 0x90, 2);
     }
 
+    if (g_EnableCaps) {
     LOG_DEBUG("ApplyPatches: stage 3 (caps)");
     Patch4(0x0077E055 + 1, 2147483646);
     Patch4(0x0077E12F + 1, 2147483646);
@@ -86,7 +143,9 @@ void ClientConfig::ApplyPatches() {
     PatchMemory((void*)((uintptr_t)0x009530FB + 2), backspinCooltime, sizeof(backspinCooltime));
     unsigned char doubleupper[] = { 0x00, 0x01 };
     PatchMemory((void*)((uintptr_t)0x00953688 + 1), doubleupper, sizeof(doubleupper));
+    }
 
+    if (g_EnableSkillTweaks) {
     LOG_DEBUG("ApplyPatches: stage 4 (skill tweaks)");
     PatchNop(0x009A4482, 0x009A4484);
 
@@ -98,7 +157,9 @@ void ClientConfig::ApplyPatches() {
     Patch1(0x0068E534 + 1, 0x86);
     Patch1(0x0068E65D + 1, 0x86);
     Patch1(0x0068E709 + 1, 0x86);
+    }
 
+    if (g_EnableWindowLogos) {
     if (WindowedMode) {
         LOG_DEBUG("ApplyPatches: windowed mode");
         unsigned char forced_window[] = { 0xb8, 0x00, 0x00, 0x00, 0x00 };
@@ -109,11 +170,14 @@ void ClientConfig::ApplyPatches() {
         LOG_DEBUG("ApplyPatches: remove logos");
         FillBytes(0x0062EE54, 0x90, 21);
     }
+    }
 
+    if (g_EnableClimbSpeed) {
     LOG_DEBUG("ApplyPatches: stage 5 (climb speed)");
     Patch4(0x009CC6F9 + 2, 0x00C1CF80);
     unsigned char climbSpeedBytes[8];
     *(double*)climbSpeedBytes = climbSpeed * 3.0f;
     PatchMemory((void*)0x00C1CF80, climbSpeedBytes, 8);
+    }
     LOG_DEBUG("ApplyPatches: done");
 }
