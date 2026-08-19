2393be92322ccff098ca9d38aba4819837745735
luckyhhe <luckyhhe@tencent.com>
2026-08-19 08:44:52 +0800

    load custom

--- src/resman.cpp
+++ src/resman.cpp
@@ -65,6 +65,73 @@ public:
 void CWvsApp::InitializeResMan_hook() {
     DEBUG_MESSAGE("CWvsApp::InitializeResMan");
     CWvsApp::InitializeResMan(this);
+// add custom namespace to root
+    IWzWritableNameSpacePtr pWritableRoot;
+    if (FAILED(get_root()->QueryInterface(&pWritableRoot))) {
+        ErrorMessage("Failed to cast root namespace");
+        return;
+    }
+    IWzNameSpacePtr pNameSpace;
+    PcCreateObject<IWzNameSpacePtr>(L"NameSpace", pNameSpace, nullptr);
+    Ztl_variant_t vResult;
+    pWritableRoot->AddObject(L"Custom", static_cast<IUnknown*>(pNameSpace), &vResult);
+    g_pCustomNameSpace = vResult.GetUnknown();
+
+    // load Custom.wz from file system
+    IWzFileSystemPtr fs;
+    PcCreateObject<IWzFileSystemPtr>(L"NameSpace#FileSystem", fs, nullptr);
+    char sStartPath[MAX_PATH];
+    GetModuleFileNameA(nullptr, sStartPath, MAX_PATH);
+    Dir_BackSlashToSlash(sStartPath);
+    Dir_upDir(sStartPath);
+    strcat_s(sStartPath, MAX_PATH, "/Custom");
+    fs->Init(Ztl_bstr_t(sStartPath));
+
+    // IWzPackagePtr pPackage;
+    // PcCreateObject<IWzPackagePtr>(L"NameSpace#Package", pPackage, nullptr);
+    // IWzSeekableArchivePtr pArchive = fs->item[L"Custom.wz"].GetUnknown();
+    // pPackage->Init(L"83", L"Custom", pArchive);
+    g_pCustomNameSpace->Mount(L"/", fs, 0);
+
+    // iterate custom namespace
+    std::vector<std::tuple<Ztl_bstr_t, IEnumVARIANTPtr>> stack;
+    stack.emplace_back(L"", g_pCustomNameSpace->_NewEnum);
+    while (!stack.empty()) {
+        auto [sPath, pEnum] = stack.back();
+        stack.pop_back();
+
+        while (true) {
+            Ztl_variant_t vNext;
+            ULONG uCeltFetched;
+            if (FAILED(pEnum->Next(1, &vNext, &uCeltFetched)) || uCeltFetched == 0) {
+                break;
+            }
+            Ztl_bstr_t sUOL = (sPath.length() > 0 ? sPath + L"/" : L"") + V_BSTR(&vNext);
+            Ztl_variant_t vObj = get_rm()->GetObjectA(L"Custom/" + sUOL);
+            IUnknownPtr pUnk = vObj.GetUnknown();
+            if (pUnk) {
+                IWzNameSpacePtr pSub;
+                if (SUCCEEDED(pUnk->QueryInterface(&pSub))) {
+                    stack.emplace_back(sUOL, pSub->_NewEnum);
+                    continue;
+                }
+                IWzPropertyPtr pProp;
+                if (SUCCEEDED(pUnk->QueryInterface(&pProp))) {
+                    stack.emplace_back(sUOL, pProp->_NewEnum);
+                }
+            }
+            g_vecOverrides.push_back(sUOL);
+        }
+    }
+    std::sort(g_vecOverrides.begin(), g_vecOverrides.end()); // uses operator<
+
+    // NameSpace.dll - try resolving from g_pCustomNameSpace
+    IWzNameSpaceImpl::raw__OnGetLocalObject_orig = static_cast<IWzNameSpaceImpl::raw__OnGetLocalObject_t>(GetAddressByPattern("NAMESPACE.DLL", "B8 ?? ?? ?? ?? E8 ?? ?? ?? ?? 81 EC 80"));
+    ATTACH_HOOK(IWzNameSpaceImpl::raw__OnGetLocalObject_orig, IWzNameSpaceImpl::raw__OnGetLocalObject_hook);
+
+    // PCOM.dll - patch CWzProperty objects during serialization
+    CWzProperty::raw_Serialize_orig = static_cast<CWzProperty::raw_Serialize_t>(GetAddressByPattern("PCOM.DLL", "B8 ?? ?? ?? ?? E8 ?? ?? ?? ?? 83 EC 68"));
+    ATTACH_HOOK(CWzProperty::raw_Serialize_orig, CWzProperty::raw_Serialize_hook);
 }
 
 void CWvsApp::CleanUp_hook() {
